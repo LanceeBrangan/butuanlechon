571df6576c73e173d854b09cdac44ffdfbe54a58
Lance <lanceebrangan@gmail.com.>
2026-05-01 12:34:46 +0800

    fix: issues on register and log-in.

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,21 @@
 <script setup>
-import { RouterView } from 'vue-router'
+import { RouterView, useRoute } from 'vue-router'
 import AppLayout from '@/components/layout/AppLayout.vue'
+import { computed } from 'vue'
+
+const route = useRoute()
+
+// Show AppLayout only for authenticated routes
+const showAppLayout = computed(() => {
+  return route.meta.requiresAuth !== false
+})
 </script>
 
 <template>
-  <AppLayout :is-with-app-bar-nav-icon="true">
+  <AppLayout v-if="showAppLayout" :is-with-app-bar-nav-icon="true">
     <template #content>
       <RouterView />
     </template>
   </AppLayout>
+  <RouterView v-else />
 </template>
